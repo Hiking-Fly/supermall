e028c23a21a48370d0a0c45bfe1995fb9b506d6e
张小飞 <2624970078@qq.com>
2022-04-24 17:55:38 +0800

    添加图片

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="nav-bar" :style='bgcolorCom'>
+      <div class="left"><slot name="left"></slot></div>
+      <div class="center"><slot name="center"></slot></div>
+      <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'NavBar',
+  props:{
+      bgcolor:String,
+  },
+  data () {
+    return {
+    };
+  },
+  components: {},
+  computed:{
+      bgcolorCom(){
+          return {
+              'background-color':this.bgcolor
+          }
+      }
+  },
+  methods: {}
+}
+</script>
+
+<style scoped>
+.nav-bar{
+    display: flex;
+    height: 44px;
+    
+}
+.center{
+    flex:1;
+    text-align: center;
+    line-height: 44px;
+}
+.right,.left{
+    width: 60px;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>首页</div>
+  <div id='home'>
+    <nav-bar>
+      <div slot="center" bgcolor='blue'>购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
-export default {
-  name:"Home",
-  data () {
-    return {
-    };
-  },
-  components: {},
-  methods: {}
-}
+  import NavBar from 'components/common/navbar/NavBar'
+  export default {
+    name:"Home",
+    data () {
+      return {
+      };
+    },
+    components: {
+      NavBar
+    },
+    methods: {}
+  }
 </script>
-
+ 
 <style scoped>
 </style>
